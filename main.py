7e9d19ad4f6912637bcbe4fa51ca79d2a0cd38eb
Chris Doan <cdoan@redhat.com>
2025-06-21 10:26:09 -0500

    try fastmcp

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP("Demo 🚀")
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
 def main():
     print("Hello from fm-mcp!")
-
+    # mcp.run()
+    mcp.run(transport="streamable-http", host="0.0.0.0", port=8000, path="/mcp")
 
 if __name__ == "__main__":
     main()
