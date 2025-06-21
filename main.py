179de5338e7ba837c270bdb273a13bb2dcf2289e
Chris Doan <cdoan@redhat.com>
2025-06-21 10:50:37 -0500

    add tools example

--- main.py
+++ main.py
@@ -7,10 +7,25 @@ def add(a: int, b: int) -> int:
     """Add two numbers"""
     return a + b
 
+@mcp.tool()
+def multiply(a: float, b: float) -> float:
+    """Multiplies two numbers together."""
+    return a * b
+
+@mcp.tool()
+def hello(name: str) -> str:
+    return f"Hello, {name}!"
+
 def main():
     print("Hello from fm-mcp!")
     # mcp.run()
-    mcp.run(transport="streamable-http", host="0.0.0.0", port=8000, path="/mcp")
+    mcp.run(
+        transport="streamable-http",
+        host="0.0.0.0",
+        port=8000,
+        path="/mcp",
+        log_level="debug",
+    )
 
 if __name__ == "__main__":
     main()
